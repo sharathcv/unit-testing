b7e622718bfaa397e7064cbfef6cdc9bf9d13ada
sharathcv <sharathcv@gmail.com>
2025-05-01 14:21:01 +1000

    Basic test setup

---
+++ unit-testing-net9-app-xUnit-net-v3/Calculations/Calculations.Test/CalculatorTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Calculations.Test
+{
+    public class CalculatorTest
+    {
+        [Fact] // The fact attribute turns it into a test method
+        public void TestAdd()
+        {
+            Assert.True(true);
+        }
+    }
+}
